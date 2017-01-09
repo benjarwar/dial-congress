571f47a4a9289626e842ef7bee7693dd05f6468f
BJ Warshaw <benjarwar@gmail.com>
2017-01-08 19:16:32 -0500

    Test initial scan and element wrapping
    - Introduces highlightRegex jQuery plugin for DOM scanning

--- js/content.js
+++ js/content.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
+  var $body = $('body');
   var senateData;
 
+  var scan = function() {
+    $.each(senateData, function(i, senator) {
+      var firstLastRegEx = new RegExp(senator.firstName + ' ' + senator.lastName);
+
+      $body.highlightRegex(firstLastRegEx, {
+        className: 'dial-congress',
+        attrs: {
+          'data-phone': senator.phone
+        }
+      });
+    });
+  }
+
   $.when(
-    $.get(chrome.extension.getURL("js/senate.json"), function(data) {
-      senateData = data;
+    $.get(chrome.extension.getURL('js/senate.json'), function(data) {
+      senateData = JSON.parse(data);
     })
   ).then(function() {
-
+    scan();
   })
 });
