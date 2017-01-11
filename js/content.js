87d47485e15c3bbfdae49f3d60ff27fe701bee21
BJ Warshaw <benjarwar@gmail.com>
2017-01-10 20:56:21 -0500

    Add reg exp unit testing and further refactoring

--- js/content.js
+++ js/content.js
@@ -1,80 +1,75 @@
-$(document).ready(function() {
-  var className = 'dial-congress';
-  var i = 0;
-  var senateData;
-  var context = document.body;
-  var instance = new Mark(context);
-  var t0 = performance.now();
+var i = 0;
+var mark;
+var senateData;
 
-  function getRegExpString(senator) {
-    var title = '(Senator|((?!([A-Z0-9])).|^)Sen.|Congressman|Congresswoman)\\s*';
-    var optionalTitle = '(' + title + ')?';
-    var wildCardMiddle = '\\s*?(\'|")?(?:\\w*).?(\'|")?\\s*?';
-    var upToTwoWildCardMiddles = '\\s' + wildCardMiddle + wildCardMiddle;
-    var firstLast = optionalTitle + senator.firstName + upToTwoWildCardMiddles + senator.lastName;
-    var titleLast = title + senator.lastName;
-    var lastFirst = senator.lastName + ',\\s*' + senator.firstName;
-    var nicknames = '';
+function getRegExp(senator) {
+  var title = '(Senator|((?!([A-Z0-9])).|^)Sen.|Congressman|Congresswoman)\\s*';
+  var optionalTitle = '(' + title + ')?';
+  var wildCardMiddle = '\\s*?(\'|")?(?:\\w*).?(\'|")?\\s*?';
+  var upToTwoWildCardMiddles = '\\s' + wildCardMiddle + wildCardMiddle;
+  var firstLast = optionalTitle + senator.firstName + upToTwoWildCardMiddles + senator.lastName;
+  var lastFirst = senator.lastName + ',\\s*' + senator.firstName;
+  var titleLast = title + senator.lastName;
+  var nicknames = '';
+  var regExpString = '';
 
-    function getNicknameString (nickname, lastName) {
-      return '|' + optionalTitle + nickname + upToTwoWildCardMiddles + lastName + '|' + senator.lastName + ',\\s*' + nickname;
-    }
+  function getNicknameString (nickname, lastName) {
+    return '|' + optionalTitle + nickname + upToTwoWildCardMiddles + lastName + '|' + senator.lastName + ',\\s*' + nickname;
+  }
 
-    if (senator.nicknames) {
-      if (Array.isArray(senator.nicknames)) {
-        for (var i = 0; i < senator.nicknames.length; i++) {
-          nicknames += getNicknameString(senator.nicknames[i], senator.lastName);
-        }
-      } else if (typeof senator.nicknames === 'string') {
+  if (senator.nicknames) {
+    if (Array.isArray(senator.nicknames)) {
+      for (var i = 0; i < senator.nicknames.length; i++) {
         nicknames += getNicknameString(senator.nicknames[i], senator.lastName);
       }
-    }
-
-    return firstLast + '|' + lastFirst + '|' + titleLast + nicknames;
-  }
-
-  function scanForSenator(i) {
-    var senator = senateData[i];
-    var regExpString = getRegExpString(senator);
-    var regExp = new RegExp(regExpString, 'ig');
-
-    console.log('looking for ' + senator.firstName + ' ' + senator.lastName);
-
-    instance.markRegExp(regExp, {
-      element: 'span',
-      className: className,
-      each: function(el) {
-        buildTooltip(el, senator);
-      }
-    });
-  }
-
-  function buildTooltip(el, senator) {
-    el.setAttribute('title', senator.party + '/' + senator.state + ': ' + senator.phone);
-
-    $(el).tooltipster({
-      interactive: true,
-      theme: ['tooltipster-noir', 'tooltipster-noir-customized']
-    });
-  }
-
-  function scan() {
-    scanForSenator(i);
-
-    if (i < senateData.length - 1) {
-      i++;
-      setTimeout(scan, 5);
-    } else {
-      console.log('dial-congress total scan time: ' + (performance.now() - t0));
+    } else if (typeof senator.nicknames === 'string') {
+      nicknames += getNicknameString(senator.nicknames[i], senator.lastName);
     }
   }
 
+  regExpString += firstLast + '|' + lastFirst + '|' + titleLast + nicknames;
+
+  return new RegExp(regExpString, 'ig');
+}
+
+function scanForSenator(i) {
+  var senator = senateData[i];
+  var regExp = getRegExp(senator);
+
+  mark.markRegExp(regExp, {
+    element: 'span',
+    className: 'dial-congress',
+    each: function(el) {
+      buildTooltip(el, senator);
+    }
+  });
+}
+
+function buildTooltip(el, senator) {
+  el.setAttribute('title', senator.party + '/' + senator.state + ': ' + senator.phone);
+
+  $(el).tooltipster({
+    interactive: true,
+    theme: ['tooltipster-noir', 'tooltipster-noir-customized']
+  });
+}
+
+function scan() {
+  scanForSenator(i);
+
+  if (i < senateData.length - 1) {
+    i++;
+    setTimeout(scan, 5);
+  }
+}
+
+$(document).ready(function() {
   $.when(
     $.get(chrome.extension.getURL('js/senate.json'), function(data) {
       senateData = JSON.parse(data);
     })
   ).then(function() {
-    console.log('dial-congress scanning...');
+    mark = new Mark(document.body);
     scan();
   })
 });
---
+++ test/test.js
@@ -0,0 +1,128 @@
+var expect = require('chai').expect;
+var fs = require('fs');
+var path = require('path');
+
+var document = {};
+var $ = function(context) {
+  if (this.__proto__.constructor !== $) {
+    return new $(context);
+  }
+
+  this.context = context;
+  this.ready = function() {}
+};
+
+contentJavascript = fs.readFileSync(path.resolve(__dirname, '../js/content.js'),'utf8');
+eval(contentJavascript);
+
+describe('getRegExpString', function() {
+  var senator =   {
+    'firstName': 'Bernard',
+    'lastName': 'Sanders',
+    'nicknames': [
+      'Bernie',
+      'Barnie'
+    ]
+  };
+
+  var re = getRegExp(senator);
+
+  it('matches first, middle, and last name permutations', function() {
+    expect('Bernard Sanders'.match(re).length).to.equal(1);
+    expect('Bernard Birdie Sanders'.match(re).length).to.equal(1);
+    expect('Bernard Birdie Bird Sanders'.match(re).length).to.equal(1);
+    expect('Bernard B. Sanders'.match(re).length).to.equal(1);
+  });
+
+  it('matches varius nickname permutations', function() {
+    expect('Bernie Sanders'.match(re).length).to.equal(1);
+    expect('Bernie Birdie Sanders'.match(re).length).to.equal(1);
+    expect('Bernie Birdie Bird Sanders'.match(re).length).to.equal(1);
+    expect('Bernie B. Sanders'.match(re).length).to.equal(1);
+    expect('Barnie Sanders'.match(re).length).to.equal(1);
+    expect('Barnie Birdie Sanders'.match(re).length).to.equal(1);
+    expect('Barnie Birdie Bird Sanders'.match(re).length).to.equal(1);
+    expect('Barnie B. Sanders'.match(re).length).to.equal(1);
+  });
+
+  it('matches quoted nicknames', function() {
+    expect('Bernard "Birdie" Sanders'.match(re).length).to.equal(1);
+    expect('Bernard \'Birdie\' Sanders'.match(re).length).to.equal(1);
+    expect('Bernie "Birdie" Sanders'.match(re).length).to.equal(1);
+    expect('Bernie \'Birdie\' Sanders'.match(re).length).to.equal(1);
+    expect('Barnie "Birdie" Sanders'.match(re).length).to.equal(1);
+    expect('Barnie \'Birdie\' Sanders'.match(re).length).to.equal(1);
+    expect('Bernard \'Birdie Bee\' Sanders'.match(re).length).to.equal(1);
+    expect('Bernard \'Birdie Bee\' Sanders'.match(re).length).to.equal(1);
+    expect('Bernie \'Birdie Bee\' Sanders'.match(re).length).to.equal(1);
+    expect('Bernie \'Birdie Bee\' Sanders'.match(re).length).to.equal(1);
+  });
+
+  it('matches various title permutations', function() {
+    expect('Senator Bernard Sanders'.match(re).length).to.equal(1);
+    expect('Sen. Bernard Sanders'.match(re).length).to.equal(1);
+    expect('Congressman Bernard Sanders'.match(re).length).to.equal(1);
+    expect('Congresswoman Bernard Sanders'.match(re).length).to.equal(1);
+    expect('Senator Sanders'.match(re).length).to.equal(1);
+    expect('Sen. Sanders'.match(re).length).to.equal(1);
+    expect('Congressman Sanders'.match(re).length).to.equal(1);
+    expect('Congresswoman Sanders'.match(re).length).to.equal(1);
+  });
+
+  it('matches various title and nickname permutations', function() {
+    expect('Senator Bernie Sanders'.match(re).length).to.equal(1);
+    expect('Sen. Bernie Sanders'.match(re).length).to.equal(1);
+    expect('Congressman Bernie Sanders'.match(re).length).to.equal(1);
+    expect('Congresswoman Bernie Sanders'.match(re).length).to.equal(1);
+    expect('Senator Barnie Sanders'.match(re).length).to.equal(1);
+    expect('Sen. Barnie Sanders'.match(re).length).to.equal(1);
+    expect('Congressman Barnie Sanders'.match(re).length).to.equal(1);
+    expect('Congresswoman Barnie Sanders'.match(re).length).to.equal(1);
+    expect('Senator Bernie "Birdie" Sanders'.match(re).length).to.equal(1);
+    expect('Sen. Bernie "Birdie" Sanders'.match(re).length).to.equal(1);
+    expect('Congressman Bernie "Birdie" Sanders'.match(re).length).to.equal(1);
+    expect('Congresswoman Bernie "Birdie" Sanders'.match(re).length).to.equal(1);
+    expect('Senator Bernie "Birdie" B. Sanders'.match(re).length).to.equal(1);
+    expect('Sen. Bernie "Birdie" B. Sanders'.match(re).length).to.equal(1);
+    expect('Congressman Bernie "Birdie" B. Sanders'.match(re).length).to.equal(1);
+    expect('Congresswoman Bernie "Birdie" B. Sanders'.match(re).length).to.equal(1);
+    expect('Senator Bernie \'Birdie\' Sanders'.match(re).length).to.equal(1);
+    expect('Sen. Bernie \'Birdie\' Sanders'.match(re).length).to.equal(1);
+    expect('Congressman Bernie \'Birdie\' Sanders'.match(re).length).to.equal(1);
+    expect('Congresswoman Bernie \'Birdie\' Sanders'.match(re).length).to.equal(1);
+    expect('Senator Bernie \'Birdie\' B. Sanders'.match(re).length).to.equal(1);
+    expect('Sen. Bernie \'Birdie\' B. Sanders'.match(re).length).to.equal(1);
+    expect('Congressman Bernie \'Birdie\' B. Sanders'.match(re).length).to.equal(1);
+    expect('Congresswoman Bernie \'Birdie\' B. Sanders'.match(re).length).to.equal(1);
+  });
+
+  it('matches multiple occurences', function() {
+    var string = 'Let me be as clear as Bernard Sanders can be. This election is not about and has never been about Hillary Clinton or Donald Trump or Bernie Sanders or Senator Sanders or Bernie "Birdie" Sanders or any of the other candidates who sought the presidency. This election is not about political gossip, it\'s not about polls, it\'s not about campaign strategy, it is not about Sen. Barnie, it is not about Congressman Bernard, it is not about all the things that the media spends so much time discussing.';
+
+    expect(string.match(re).length).to.eql(4);
+  });
+
+  it('does not match single names on their own', function() {
+    expect('Bernard'.match(re)).to.be.null;
+    expect('Sanders'.match(re)).to.be.null;
+    expect('Bernie'.match(re)).to.be.null;
+    expect('Barnie'.match(re)).to.be.null;
+    expect('I love Bernie'.match(re)).to.be.null;
+    expect('Colonel Sanders'.match(re)).to.be.null;
+    expect('Bernie would have won.'.match(re)).to.be.null;
+    expect('But Barnie wouldn\'t have won.'.match(re)).to.be.null;
+  });
+
+  it('does not match misspellings', function() {
+    expect('Bernerd Sanders'.match(re)).to.be.null;
+    expect('Bernie Sunders'.match(re)).to.be.null;
+  });
+
+  it('does not match more than two middle words', function() {
+    expect('Bernard Might Have Won Sanders'.match(re)).to.be.null;
+  });
+
+  it('does not match abbreviated "sen." at end of previous sentence', function() {
+    expect('He wore the nicest lederhosen. Sanders is a nice guy.'.match(re)).to.be.null;
+  });
+});
