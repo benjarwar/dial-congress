0751e057ccc7c97e4323507b00c2abc33eb6dc60
Greg Lavallee <greg.lavallee@slate.com>
2017-02-14 21:57:36 -0500

    Mutation observer

--- js/content.js
+++ js/content.js
@@ -117,6 +117,31 @@ function buildTooltip($el, senator) {
   $el.tooltipster('open');
 }
 
+
+// https://davidwalsh.name/mutationobserver-api
+(function(){
+
+var observer = new MutationObserver(function(mutations) {
+	// For the sake of...observation...let's output the mutation to console to see how this all works
+	mutations.forEach(function(mutation) {
+		console.log(mutation.type);
+	});
+});
+
+// Notify me of everything!
+var observerConfig = {
+	attributes: true,
+	childList: true,
+	characterData: true
+};
+
+// Node, config
+// In this case we'll listen to all changes to body and child nodes
+var targetNode = document.body;
+observer.observe(targetNode, observerConfig);
+
+})();
+
 function track(data) {
   chrome.runtime.sendMessage(data, function(response) {
     // console.log('message received', response);
